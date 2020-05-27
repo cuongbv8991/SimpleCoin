0924b919f40b99a0b398413ec5db3c61cc5778ee
cuongbv2 <cuongbv2@vng.com.vn>
2020-05-27 22:19:32 +0700

    add scan qr code

--- app/src/main/java/com/hcmus/simplecoin/main/BalanceActivity.kt
+++ app/src/main/java/com/hcmus/simplecoin/main/BalanceActivity.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.google.zxing.integration.android.IntentIntegrator
 import com.hcmus.simplecoin.R
@@ -12,6 +13,7 @@ import com.hcmus.simplecoin.receivecoin.ReceiveCoinActivity
 import com.hcmus.simplecoin.utils.CoinManager
 import com.hcmus.simplecoin.utils.showShortToast
 import kotlinx.android.synthetic.main.activity_main.*
+
 
 class BalanceActivity : AppCompatActivity(), BalanceView {
 
@@ -35,6 +37,31 @@ class BalanceActivity : AppCompatActivity(), BalanceView {
     private fun initListener() {
         btnReceive.setOnClickListener {
             startActivity(ReceiveCoinActivity.intentFor(this))
+        }
+
+        btnSend.setOnClickListener {
+            val integrator = IntentIntegrator(this)
+            integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE)
+            //integrator.setPrompt("Scan a barcode")
+            integrator.setCameraId(0) // Use a specific camera of the device
+
+            integrator.setBeepEnabled(false)
+            integrator.setBarcodeImageEnabled(true)
+            integrator.initiateScan()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        val result =
+            IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
+        if (result != null) {
+            if (result.contents == null) {
+                Log.d("decode_qr_code", "Cancelled")
+            } else {
+                Log.d("decode_qr_code", "Scanned: " + result.contents)
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data)
         }
     }
 
---
+++ app/src/main/java/com/hcmus/simplecoin/sendcoin/SendCoinActivity.kt
@@ -0,0 +1,13 @@
+package com.hcmus.simplecoin.sendcoin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.hcmus.simplecoin.R
+
+class SendCoinActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_send_coin)
+    }
+}
